da581dd018a2960ea344459a900956260fb1cbff
evaristoc <varikvi@yahoo.com>
2014-05-15 14:38:58 +0200

    updates

--- phase02/README.md
+++ phase02/README.md
@@ -1,33 +1,37 @@
 ONLINE PANEL SIMULATION - SECOND PHASE - Outline
 
-Keeping the same objectives as in the first phase, in this second phase I will continue exploring the simulation. This time subsequent functions, behaviours and attributes will be added to better fit some current real practices but also some simplifications. The IBRO model (see previous phase) could be emphasised but the general approach to sampling will be modified and upgraded to include certain dynamics found in field.
-Some observations are:
-
-1) IBRO (for Inflow - Base - Retention/churn - Outflow) model
-
-- Functions better supported by empirical observations to explain survival and response rates per cohort will be used.
-- The baseline for a population would be evaluated in terms of a year-time-horizon proposal, counted in weeks (not days, as previous phase).
-
-2) Sampling Simulations
-
-- Previous simulation was based on an invitation model of "one invite, one survey". For this phase the now more popular Inbox and Router models will be suggested. This may involve a new class or method to simulate the Inbox per panelist.
-- For the moment the sampling analysis and the behaviour of the population during the sampling event will be evaluated in weeks, no days as in Phase 01.
-- So far the simulated surveys will consist only in ad-hoc ones, but this could be easily modified to include simulations for longitudinal studies of any kind, as well as exclusions.
-
-EDIT 09-05-2014:
+This script is more advanced than the previous phase, and although won't easily scale to a large amount of records without affecting performance, the use of OO approach will ease readability, possible extensions and improvements, and the maintenance of the current script.
 
 A first (still uncompleted) script file was loaded:
 - the script gives an idea of the direction of the major implementation
 - differently to previous phase, during this phase the project uses Object Oriented programming as main programming paradigm, which is more suitable to the model characteristics 
 
+Keeping the same objectives as in the first phase, in this second phase I will continue exploring the simulation. This time subsequent functions, behaviours and attributes will be added to better fit some current real practices but also some simplifications. The IBRO model (see previous phase) could be emphasised but the general approach to sampling will be modified and upgraded to include certain dynamics found in field.
+Some observations are:
+
+1. IBRO (for Inflow - Base - Retention/churn - Outflow) model
+
+- Functions better supported by empirical observations to explain survival and response rates per cohort will be used.
+- The baseline for a population would be evaluated in terms of a year-time-horizon proposal, counted in weeks (not days, as previous phase).
+
+2. Sampling Simulations
+
+- Previous simulation was based on an invitation model of "one invite, one survey". For this phase the now more popular Inbox and Router models will be suggested. This may involve a new class or method to simulate the Inbox per panelist.
+- For the moment the sampling analysis and the behaviour of the population during the sampling event will be evaluated in weeks, no days as in Phase 01.
+- So far the simulated surveys will consist only in ad-hoc ones, but this could be easily modified to include simulations for longitudinal studies of any kind, as well as exclusions.
+
+EDIT 15-05-2014:
+
+A first quick test of the currently available classes and functions has been included.
+
 Apart of what it is shown, additional tasks would include:
-- This time, Panellists will interact with Surveys through simulated (online) Portals during each Sampling procedure
+- Rescuing the concept of "exposure rate" given in the previous definition as a black-box measure approximation to the effects of technological change
+- Simple Diagrams of the simulations (currently unavailable)
 - A threading procedure for the sampling Event
 - Likely also a threading for the universe generation during updating of Panellist's instances status
 
-This script is more advanced than the previous phase, and although won't easily scale to a large amount of records without affecting performance, the use of OO approach will ease readability, possible extensions and improvements, and the maintenance of the current script
-
 PS: Phases 01 and 02 are working as the base to investigate the details of relevant behaviours and parameters. A possible third phase would re-evaluate the results and parameters found at phases 01 and 02 seeking a simplified scalable model to reproduce more data-intensive simulations.  
+PSS: Currently thinking about creating a web-based interface to interact with the model instead... 
 
 Participants: E Caraballo, conception and developer
 
--- phase02/simulation_v2.041.py
+++ phase02/simulation_v2.041.py
@@ -7,17 +7,7 @@ import itertools
 import operator
 import copy
 import inspect
-
-#max_indid = [-1]
-#divisor_beta_1 = [-1,-1]
-#divisor_beta_2 = [-1,-1]
-#amount_topurge = -1
-#wished_rr = .28
-#univ = [None]
-#purged = [None]
-#week = 0
-#exposure_rate = 0
-#pbe = 0
+import cProfile
 
 # TODO:
 # - Start solving Panelist so he can keep a list of the projects he has finished
@@ -26,9 +16,6 @@ import inspect
 # - Prepare the completion model and how to access it
 # ----- This includes the threading process!!! So get it ready...
 # - Prepare the universe creation model (RELEVANT!)
-# OJO: https://docs.python.org/3/library/functions.html#property
-# http://stackoverflow.com/questions/16025462/what-is-the-right-way-to-put-a-docstring-on-python-property
-
 
 class counter(object):
         '''
@@ -39,13 +26,10 @@ class counter(object):
         def __init__(self, start):
                 self.state = start
         def __call__(self, week):
-#         print(week, self.state) #apparently modifies stdout!!!
                 if self.state:
                         self.state -= 1
                 if self.state == 0:
                         self.state = None
-
-
 
 class Panelist(object):
         '''
@@ -53,6 +37,8 @@ class Panelist(object):
         # Attributes are defined in terms of response and panel life cycle characterisation
         # Additional attributes could be added
         '''
+        superweek = 0
+        
         def active_tenure(self, kappa_scale = 0.73, lambda_shape = 0.44):
                 '''
                 Survival in weeks
@@ -60,14 +46,20 @@ class Panelist(object):
                 --- 1/kappa: scale parameter alpha in Python (kappa = 0.73)
                 --- lambda: shape parameter beta in Python (lambda = 0.44)
                 '''
-                self.act_ten = int(random.weibullvariate(1/kappa_scale,lambda_shape))
-                return self.act_ten   
+                self.act_ten = 1
+                self.act_ten += round(random.weibullvariate(1/kappa_scale,lambda_shape))
+                #return self.act_ten 
         getacttenure = property(active_tenure, doc = 'assigned act tenure as weibull random variate')
-        def __init__(self, indid = -1):
+
+        def __init__(self, indid = -1, week = -1):
                 self.indid = indid
+                self.startweek = week
                 self.getacttenure
-                self.touchedsurvs = []
+                self.touchedsurvs = [('survid','week','touchstatus (1=complete, 2=other)')]
                 self.status = True
+                self.inact_period = random.random()
+                self.startinactten = -1
+                self.startpurge = -1
         def rr_func(self, kf_a1 = -0.079,  kf_a2 = 1.5e-3, kf_b = 1.70, mhuf_a = 0.014, mhuf_b = 0.54):
                 '''
                 The function associated to the assigned lifetime
@@ -84,18 +76,7 @@ class Panelist(object):
                 negbinp = negbin_par(self.k,self.mhu)
                 return negbinp
         getrr = property(rr_func, doc = 'parameters of neg bin function based on active tenure and empirical behaviour')
-        def visit_rr(self, p = None):
-                '''
-                Should be a random variable based on active tenure and rr
-                '''
-                pass
-        def qstatus(self, qstatus = None):
-                '''
-                The last touching status variable
-                Should be reinitiated after leaving the portal
-                '''
-                self.qstatus = qstatus
-        def cumnegbin(s, p, n):
+        def cumnegbin(self, s, p, n):
                 '''
                 For the first demo I will use the cum dist of the negbin as in:
                 #http://stackoverflow.com/questions/1095650/how-can-i-efficiently-calculate-the-binomial-cumulative-distribution-function
@@ -110,7 +91,7 @@ class Panelist(object):
                 for j in range(a, c + 1):
                         prob += math.factorial(c) / (math.factorial(j)*math.factorial(c-j)) * x**j * (1 - x)**(c-j)
                 return prob
-        def touch_func(num_surs):
+        def touch_func(self, num_surs):
                 '''
                 The model takes the cumnegbin dist and use an accept/reget method compared to a uniform random variable
                 by calculating succesive values along the cumnegbin until the dist is higher or equal to
@@ -129,12 +110,12 @@ class Panelist(object):
                 --- effect of past experience; professionalism
                 '''
                 comparing_val = random.uniform(0,1)
-                p = self.k/(self.k+self.mhu)
+                p = self.getrr.k/(self.getrr.k+self.getrr.mhu)
                 num_touch = 0
-                if comparing_val >= cumnegbin(0, p, num_surs):
+                if comparing_val >= self.cumnegbin(0, p, num_surs):
                         num_touch += 1
                         for ns in range(1,num_surs+1):
-                                if comparing_val > (cumnegbin(ns,p,num_surs)-cumnegbin(ns-1,p,num_surs))/(cumnegbin(num_surs,p,num_surs)-cumnegbin(ns-1,p,num_surs)):
+                                if comparing_val > (self.cumnegbin(ns,p,num_surs)-self.cumnegbin(ns-1,p,num_surs))/(self.cumnegbin(num_surs,p,num_surs)-self.cumnegbin(ns-1,p,num_surs)):
                                         num_touch += 1
                                 else:
                                         break
@@ -145,7 +126,7 @@ class Panelist(object):
                 '''
                 if self.status == True:
                         num_surs = len(updated_portal)
-                        num_touch = touch_func(num_surs)
+                        num_touch = self.touch_func(num_surs)
                         if num_touch > 0:
                                 for s in range(num_touch):
                                         if updated_portal[s].exp_incidence >= random.random():
@@ -153,20 +134,25 @@ class Panelist(object):
                                                 updated_portal[s].ch_completes(1)
                                         else:
                                                 self.touchedsurvs.append((updated_portal[s].survid, week, 2))
-                return self.touchedsurvs
-        def dead_tenure(self):
+        def inactive_tenure(self):
                 '''
                 Applying the model for dead tenure
                 self.status = False is for dead
                 '''
-                self.status = False
-        def purged(self, purged = None):
+                
+                if Panelist.superweek - self.startweek == self.act_ten:
+                        self.status = False
+                        self.startinactten = Panelist.superweek
+        setinactten = property(inactive_tenure)
+        def purged(self):
                 '''
                 Making the purging based on dead tenure
                 self.status = None is for purged
                 '''
-                self.status = None
-                #self.purged = purged
+                if self.status == False and ((Panelist.superweek - self.startinactten == 5 and self.inact_period <= .5) or (Panelist.superweek - self.startinactten == 18 and self.inact_period >= .5)):
+                        self.status = None
+                        self.startpurge = Panelist.superweek
+        setpurged = property(purged)
         def recruit (self, recruit = None):
                 self.recruit = recruit
         def pstatus(self, pstatus = None):
@@ -177,9 +163,17 @@ class Panelist(object):
                 self.participation = participation
         def batch(self, batch):
                 self.batch = -77
-
-
-
+        def visit_rr(self, p = None):
+                '''
+                Should be a random variable based on active tenure and rr
+                '''
+                pass
+        def qstatus(self, qstatus = None):
+                '''
+                The last touching status variable
+                Should be reinitiated after leaving the portal
+                '''
+                self.qstatus = qstatus
 
 class survey_study(object):
         '''
@@ -190,27 +184,30 @@ class survey_study(object):
         Could eventually include a difference between trackers and ad-hocs
         /// OJO: Previous included an expiration date but the most recent one will be just instantaneous! ///
         '''
-        def __init__(self, survid = 0, exp_completes = 100, exp_incidence = .20, exp_fw = 4, status = True):
+        def __init__(self, survid, start_week, exp_completes = 100, exp_incidence = .20, exp_fw = 1, status = True):
+                '''
+                originally the initial settings were:
+                survid = 0, exp_completes = 100, exp_incidence = .20, exp_fw = 4, status = True
+                I changed exp_fw for = 1...
+                '''
                 self.survid = survid
+                # start_week: temporary solution to completing update
+                self.start_week = start_week
+                self.exp_fw = exp_fw
                 self.ch_completes = counter(exp_completes)
-                self.ch_fw = counter(exp_fw)
+                self.ch_fw = counter(self.exp_fw)
                 self.exp_completes = exp_completes
                 self.exp_incidence = exp_incidence
                 self.status = status
-        def completing(self, Panelist, specs):
+        def completing(self, current_week):
                 '''
-                /// CHECK!!! THIS WILL CHANGE AS THE UPDATE WILL BE MADE AT PANELIST CLASS!!
-                    ALSO, qstatus WILL NOT BE MORE A STATUS FORMAT FOR THIS PROJECT, only 1 or 2
+                Completing is occurring at Panelist! Panelist will save the information about this aspect
+                For this method I will only update the status of the survey for the moment...
                 '''
-                if Panelist.qstatus() != 'L':
-                        specs(self).exp_completes(Panelist.qstatus)
-                if week:
-                        specs(self).exp_fw(week)
-                if specs(self).exp_completes() == 0 or specs(self).exp_fw() == 0:
+                if current_week > self.start_week:
+                        self.ch_fw(current_week)
+                if self.ch_completes.state == None or self.ch_fw.state == None:
                         self.status = False
- 
-
-
 
 class full_inbox(survey_study):
         '''
@@ -230,79 +227,57 @@ class full_inbox(survey_study):
                                 if i not in self.survey_studies:
                                         self.survey_studies.append(i)
                 return self.survey_studies
+        def update_week_studies(self, current_week):
+                if self.survey_studies:
+                        for ss in self.survey_studies:
+                                ss.completing(current_week)        
         def active_studies(self):
                 '''
                 find the first True (active) in the list and report the value...
+                it should be ACTINBOX + the new surveys!!!! Otherwise I will be making longer this list!!!
                 '''
-                #http://stackoverflow.com/questions/403421/how-to-sort-a-list-of-objects-in-python-based-on-an-attribute-of-the-objects
-                #OJO: it is an IN-PLACE form of sorting!!!, so cannot be assigned!!!
                 sort_active = self.survey_studies[:]
                 sort_active.sort(key=lambda stu: stu.status)
-                # eventually a try-except here: active surveys could be exhausted at a point!!!
-                self.actinbox = copy.copy(sort_active[next((index for index, active in enumerate(sort_active) if active.status==True), None):])
+                try:
+                        self.actinbox = copy.copy(sort_active[next((index for index, active in enumerate(sort_active) if active.status==True)):])
+                except StopIteration:
+                        self.actinbox = []
                 return self.actinbox
-        #def exclude(self, survey_study):
-        #        if survey_study.completing.status() == False:
-        #                self.survey_studies.remove(survey_study)
         p_actinbox = property(active_studies)
 
-class portal(full_inbox):
+class portal(full_inbox, Panelist):
         '''
         This class should be a SUBCLASS of INBOX! (OK)
         This sub-class is meant to represent all available surveys to the panelist
         If the survey is filled or it is unavailable it shouldn't be in the portal
         The panelist participate only on those surveys that are in the portal
         '''
-        def __init__(self, portalid, myinbox):
+        def __init__(self, portalid, inbox):
                 self.portalid = portalid
-                self.myinbox = myinbox.p_actinbox[:]
-        def up_myinbox (self, myinbox):
-                #self.myinbox = self.act_inbox[:]
-                if Panelist.touchedsurvs:
-                        current_act = next((index for index, (stat, wk) in enumerate(f_surs) if wk>=week-8), None)
+                self.myinbox = inbox.p_actinbox
+        def up_view_myinbox (self, p):
+                if p.touchedsurvs:
+                        current_act = next((index for index,(a,wk,b) in enumerate(p.touchedsurvs[1:]) if wk>=week-8), None)
                         if current_act:
-                                for touchedsurvid, touchedsurvwk in Panelist.touchedsurvs[current_act:]:
+                                for touchedsurvid, touchedsurvwk, touchedstatus in p.touchedsurvs[current_act:]:
                                         for activesurv in self.myinbox:
                                                 if touchedsurvid == activesurv.survid:
                                                         self.myinbox.remove(activesurv)
                 return self.myinbox
 
-
-
-# p1 = Panelist(1)
-### OBS:
-### With the current setting at active_tenure and rr which are not set as properties and don't have
-### a return, the only way to initialise the attributes associated to them is by an unbound calling
-### of the modules:
-##### p1.active_tenure()
-##### p1.rr()
-### So I changed the order of active_tenure to become a property assigned to __init__ without calling
-### and make getrr a property so it calculates the values at calling
-# p1.getrr
-# ss1 = survey_study(1)
-# ss2 = survey_study(2)
-# ss3 = survey_study(3)
-# NO total_inbox = full_inbox([ss1, ss2, ss3])
-# total_inbox = full_inbox()
-# total_inbox.add_studies([ss1, ss2, ss3])
-# total_inbox.p_actinbox
-# full_inbox.__dict__
-# mi1 = portal(p1)
-# ss1.status = False
-# p1.filled_surveys = [(1,'C'), (2,'C')] #temporary
-# panelists = []
-# for i in range(100):
-#       panelists.append(Panelist(i))
-# surveys = []
-# for i in range(10):
-#       surveys.append(survey_study(i))
-# total_inbox.add_studies(surveys)
-
-
 def universe():
-        pass
-
-def purging():
+        '''
+        For universe:
+        - Start with an empty list of purged people and a group of recruits
+        - If a week pasts, update the full universe
+        - Now reorder from status False to True and do the following:
+        --- Find the next False and while False, verify if purging is apropriate
+        --- Those with status None should be dumped in purged list
+        --- Now find status True and make them False if apropriate
+        - Create a fix number of recruitments per week and add them to the universe
+        - Count the following week
+        Because not using numpy this is not vectorizable...
+        '''
         pass
 
 def wk_studies():
@@ -317,5 +292,69 @@ def wk_studies():
 
 
 def sampling():
+        '''
+        Here I will implement a sampling scheme (still don't know if by periods and replacement with a yield?)
+        and a threading to take care of the sampling of the existing universe
+        The functionality could be as following:
+        - Make the sample according to panelists rr's, a wet thumb rule based on empirical observations or take the full universe
+        - Start threading lock
+        - Individually update weekly portals
+        - Calculate touches
+        - Update survey statuses and week values
+        --- If survey became False, continue without updating
+        - Open threading lock
+        Sampling occurs after every universe update
+        '''
         pass
 
+
+def test_process():
+        def study_wk_test(ss, current_week):
+                added_wk = len(ss)
+                for i in range(1,4):
+                        ss.append(survey_study(i+added_wk, current_week, exp_completes = 5, exp_fw = 1))
+                        #for l in ss:
+                        #        print(l.start_week)
+                return ss
+        #uni_test = [Panelist(i,0) for i in range(1,11)]
+        recruits = [Panelist(i,0) for i in range(1,11)]
+        for p in recruits:
+                p.rr_func()
+        uni_test = recruits[:]
+        print(uni_test[0].__dict__)
+        #uni_test[0].inactive_tenure(uni_test_wk00[0].getacttenure)
+        ss = []
+        inbox = full_inbox()
+        #ss = study_wk_test(ss, 0)
+        #inbox.add_studies(ss)
+        for week in range(1,11):
+                # studies per week
+                ss = study_wk_test(ss, week-1)
+                inbox.add_studies(ss)
+                #entryinbox = inbox.p_actinbox
+                print(len(ss))
+                # sampling
+                for p in uni_test:
+                        pportal = portal(p.indid, inbox)
+                        p.weekly_touched_surveys(week-1, pportal.up_view_myinbox(p))
+                # weekly survey status update
+                inbox.update_week_studies(week)
+                print(len(inbox.survey_studies))
+                print(len(inbox.p_actinbox))
+                # panelist status update
+                Panelist.superweek = week
+                new_round = len(uni_test)
+                for p in uni_test:
+                        if p.status == None:
+                                continue
+                        elif p.status == False:
+                                p.setpurged
+                        elif p.status == True:
+                                p.setinactten
+                recruits = [Panelist(i+new_round,week) for i in range(1,11)]
+                uni_test = uni_test+recruits
+        return inbox, uni_test
+
+
+#cProfile.run('test_process()')
+inbox, uni_test = test_process()
